26d0679dce2034c6e87130167391aee7d275c42f
Freewind <nowindlee@gmail.com>
2022-06-20 16:39:37 +0800

    typescript-react-merged-tables-demo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-TypeScript React Table Demo
+TypeScript React Merged Tables Demo
 =================================
+
+在一个表格中，组合多个table在一起，左右并列。感觉非常强大。
 
 ```
 npm install
---
+++ src/Table.tsx
@@ -0,0 +1,113 @@
+import React, {FC} from 'react'
+
+import {createTable, getCoreRowModel, useTableInstance,} from '@tanstack/react-table'
+import {Person, defaultData} from './data';
+
+import './table.css';
+
+const table = createTable().setRowType<Person>()
+
+export const defaultColumns = [
+  table.createGroup({
+    header: 'Name',
+    footer: props => props.column.id,
+    columns: [
+      table.createDataColumn('firstName', {
+        cell: info => info.getValue(),
+        footer: props => props.column.id ,
+      }),
+      table.createDataColumn(row => row.lastName, {
+        id: 'lastName',
+        cell: info => info.getValue(),
+        header: () => <span>Last Name</span>,
+        footer: props => props.column.id,
+      }),
+    ],
+  }),
+  table.createGroup({
+    header: 'Info',
+    footer: props => props.column.id,
+    columns: [
+      table.createDataColumn('age', {
+        header: () => 'Age',
+        footer: props => props.column.id,
+      }),
+      table.createGroup({
+        header: 'More Info',
+        columns: [
+          table.createDataColumn('visits', {
+            header: () => <span>Visits</span>,
+            footer: props => props.column.id,
+          }),
+          table.createDataColumn('status', {
+            header: 'Status',
+            footer: props => props.column.id,
+          }),
+          table.createDataColumn('progress', {
+            header: 'Profile Progress',
+            footer: props => props.column.id,
+          }),
+        ],
+      }),
+    ],
+  }),
+]
+
+
+export const Table: FC = () => {
+
+  const [data, setData] = React.useState(() => [...defaultData])
+  const [columns] = React.useState<typeof defaultColumns>(() => [
+    ...defaultColumns,
+  ])
+
+  const rerender = React.useReducer(() => ({}), {})[1]
+
+  const instance = useTableInstance(table, {
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <div className="p-2">
+      <table>
+        <thead>
+        {instance.getHeaderGroups().map(headerGroup => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map(header => (
+              <th key={header.id} colSpan={header.colSpan}>
+                {header.isPlaceholder ? null : header.renderHeader()}
+              </th>
+            ))}
+          </tr>
+        ))}
+        </thead>
+        <tbody>
+        {instance.getRowModel().rows.map(row => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map(cell => (
+              <td key={cell.id}>{cell.renderCell()}</td>
+            ))}
+          </tr>
+        ))}
+        </tbody>
+        <tfoot>
+        {instance.getFooterGroups().map(footerGroup => (
+          <tr key={footerGroup.id}>
+            {footerGroup.headers.map(header => (
+              <th key={header.id} colSpan={header.colSpan}>
+                {header.isPlaceholder ? null : header.renderFooter()}
+              </th>
+            ))}
+          </tr>
+        ))}
+        </tfoot>
+      </table>
+      <div className="h-4"/>
+      <button onClick={() => rerender()} className="border p-2">
+        Rerender
+      </button>
+    </div>
+  )
+};
